c592ecad3af83d73d950aeb220ff71298a25b857
Mischa Jörg <mischa@ninetyfour.ch>
2023-05-28 17:10:08 +0200

    feat(mail): send slack message upon registration

--- packages/website/mail/go.mod
+++ packages/website/mail/go.mod
@@ -1,3 +1,7 @@
 module github.com/startup-nights/functions/packages/website/mail
 
 go 1.19
+
+require github.com/slack-go/slack v0.12.2
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- packages/website/mail/main.go
+++ packages/website/mail/main.go
@@ -1,28 +1,88 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"os"
+
+	"github.com/slack-go/slack"
 )
 
-// Request takes in the user's input for the filename they want and if the type is a GET or PUT.
-type Request struct {
-	// Filename is the name of the file that will be uploaded or downloaded.
-	Filename string `json:"filename"`
+var (
+	webhook string
+)
+
+func init() {
+	if webhook = os.Getenv("WEBHOOK"); webhook == "" {
+		panic("no webhook configured")
+	}
 }
 
-// Response returns back the http code, type of data, and the presigned url to the user.
+type Request struct {
+	Company struct {
+		Name                 string   `json:"name"`
+		Website              string   `json:"website"`
+		FoundingDate         string   `json:"founding_date"`
+		LinkedIn             []string `json:"linkedin"`
+		Employees            string   `json:"employees"`
+		Pitch                string   `json:"pitch"`
+		Categories           []string `json:"categories"`
+		AdditionalCategories string   `json:"additional_categories"`
+		Logo                 string   `json:"logo"`
+		Address              struct {
+			Street  string `json:"street"`
+			ZIP     string `json:"zip"`
+			City    string `json:"city"`
+			Country string `json:"country"`
+		} `json:"address"`
+		BillingAddress struct {
+			Street  string `json:"street"`
+			ZIP     string `json:"zip"`
+			City    string `json:"city"`
+			Country string `json:"country"`
+		} `json:"address_billing"`
+	} `json:"company"`
+	Contact struct {
+		FirstName string `json:"firstname"`
+		LastName  string `json:"lastname"`
+		Email     string `json:"email"`
+		Phone     string `json:"phone"`
+		Role      string `json:"role"`
+	} `json:"contact"`
+	Varia struct {
+		Package struct {
+			Title string `json:"title"`
+		} `json:"package"`
+		Formats      []string `json:"formats"`
+		Accomodation string   `json:"accomodation"`
+		Equipment    string   `json:"equipment"`
+	} `json:"varia"`
+}
+
 type Response struct {
-	// StatusCode is the http code that will be returned back to the user.
-	StatusCode int `json:"statusCode,omitempty"`
-	// Headers is the information about the type of data being returned back.
-	Headers map[string]string `json:"headers,omitempty"`
-	// Body will contain the presigned url to upload or download files.
-	Body string `json:"body,omitempty"`
+	StatusCode int               `json:"statusCode,omitempty"`
+	Headers    map[string]string `json:"headers,omitempty"`
+	Body       string            `json:"body,omitempty"`
 }
 
 func Main(in Request) (*Response, error) {
+	data, err := json.MarshalIndent(in, "", "  ")
+	if err != nil {
+		return &Response{StatusCode: http.StatusInternalServerError, Body: "{}"}, nil
+	}
+
+	if err := slack.PostWebhook(webhook, &slack.WebhookMessage{
+		Channel: "10_01_booth_notification",
+		Text:    string(data),
+	}); err != nil {
+		return &Response{StatusCode: http.StatusInternalServerError, Body: "{}"}, nil
+	}
+
 	return &Response{
 		StatusCode: http.StatusOK,
-		Body:       "",
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: "{}",
 	}, nil
 }
